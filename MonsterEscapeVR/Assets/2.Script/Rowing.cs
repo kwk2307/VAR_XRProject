6a2014df80c84fb4d9c84055b9e686c899cc8044
kwk2307 <61642951+kwk2307@users.noreply.github.com>
2021-09-06 17:21:50 +0900

    Update Rowing.cs

--- MonsterEscapeVR/Assets/2.Script/Rowing.cs
+++ MonsterEscapeVR/Assets/2.Script/Rowing.cs
@@ -10,7 +10,7 @@ public class Rowing : MonoBehaviour
     [SerializeField] private float speed = 1;
 
     private float isRowing;
-    private float isRowingPre;
+    private float isRowingPre = 0;
 
     private int a = 1;
 
@@ -21,23 +21,30 @@ public class Rowing : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //isRowing = tracker01.transform.position.x  - tracker04.transform.position.x;
         isRowing = Vector3.Distance(tracker01.transform.position, tracker04.transform.position);  //현 프레임
         //print("isRowing : " + isRowing);
 
-        if (isRowing - isRowingPre > 1)
+        if(isRowingPre == 0)
+        {
+            //isRowingPre 일때를 처리해 준다.
+            isRowingPre = isRowing;
+            return;
+        }
+
+        if (isRowing - isRowingPre > 0.7)
         {
             return; // 위치값이 튀는 것에 대한 예외처리
         }
 
         //로잉기를 당기는지 확인
-        //print("트래커의 변화 정도" + (isRowing - isRowingPre));
-
+        print("트래커의 변화 정도" + (isRowing - isRowingPre));
         if ((isRowing - isRowingPre) > 0)
         {
-            this.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * isRowing * speed);
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * (isRowing - isRowingPre) * speed);
         }
 
-        isRowingPre = Vector3.Distance(tracker01.transform.position, tracker04.transform.position); // 과거 프레임(다음 프레임 입장에서)
+        isRowingPre = isRowing; // 과거 프레임(다음 프레임 입장에서)
         //print("isRowingPre : " + isRowingPre);
     }
 
