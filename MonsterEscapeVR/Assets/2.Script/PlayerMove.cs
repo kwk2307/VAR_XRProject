332d86392a04af1379145b25919db47af6a98908
yeongjin <yunggini7676@gmail.com>
2021-09-06 10:47:56 +0900

    영
    노젓기 구현중

--- MonsterEscapeVR/Assets/2.Script/PlayerMove.cs
+++ MonsterEscapeVR/Assets/2.Script/PlayerMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rowing : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     public GameObject tracker01;
     public GameObject tracker04;
@@ -16,7 +16,7 @@ public class Rowing : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -24,20 +24,18 @@ public class Rowing : MonoBehaviour
     {
 
         curPos = this.gameObject;
+        Debug.Log("test");
 
-        if()
-        prePos = this.gameObject;
+        if (prePos == null) return;  // 첫 프레임 예외처리
 
 
         isRowing = Vector3.Distance(curPos.transform.position, prePos.transform.position);  //현 프레임
-
+        print(1);
 
         //로잉기를 당기는지 확인
-        print(isRowing - isRowingPre);
         if ((isRowing - isRowingPre) > 0)
         {
             {
-                print("들어옴");
                 transform.position += new Vector3(1, 0, 0) * distance; // 당긴 것에 비례해 z축으로 이동
 
             }
@@ -46,8 +44,8 @@ public class Rowing : MonoBehaviour
 
 
         distance = Vector3.Distance(tracker01.transform.position, tracker04.transform.position);
-        
-        
+        print(distance);
+
 
 
         prePos = this.gameObject;
