78e4ec1db12a298e50885a3e06ccb35f0e044e29
kwk2307 <61642951+kwk2307@users.noreply.github.com>
2021-09-06 15:02:38 +0900

    update: player move
    update: player move

--- MonsterEscapeVR/Assets/2.Script/Rowing.cs
+++ MonsterEscapeVR/Assets/2.Script/Rowing.cs
@@ -22,7 +22,15 @@ public class Rowing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(this.GetComponent<Rigidbody>().velocity.x > 0)
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(-1, 0, 0));
+        }
+        else
+        {
+            this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+
         isRowing = Vector3.Distance(tracker01.transform.position, tracker04.transform.position);  //현 프레임
         //print("isRowing : " + isRowing);
 
@@ -30,25 +38,24 @@ public class Rowing : MonoBehaviour
         {
             return; // 위치값이 튀는 것에 대한 예외처리
         }
-
-
-
+        
         //로잉기를 당기는지 확인
         //print("트래커의 변화 정도" + (isRowing - isRowingPre));
-        this.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * distance);
-
-        if ((isRowing - isRowingPre) > 0.03)
+       
+        if ((isRowing - isRowingPre) > 0)
         {
-            {
-                //print(isRowing - isRowingPre);
-                //transform.position += new Vector3(1, 0, 0) * distance; // 당긴 것에 비례해 z축으로 이동  
-            }
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * isRowing * speed);
         }
-        
-        distance = Vector3.Distance(tracker01.transform.position, tracker04.transform.position);
-        
+  
         isRowingPre = Vector3.Distance(tracker01.transform.position, tracker04.transform.position); // 과거 프레임(다음 프레임 입장에서)
         //print("isRowingPre : " + isRowingPre);
+    }
 
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 60, 30), "버튼"))
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * 300);
+        }
     }
 }
