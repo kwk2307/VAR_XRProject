d68348e96eaf2de916128c574ce611ef52a4c793
yeongjin <yunggini7676@gmail.com>
2021-09-06 15:03:33 +0900

    ㅇㅇ

--- MonsterEscapeVR/Assets/2.Script/GetSpeed.cs
+++ MonsterEscapeVR/Assets/2.Script/GetSpeed.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetSpeed : MonoBehaviour
 {//이 스크립트가 붙은 게임오브젝트의 속도를 구함
 
     Transform currentPosition;
     Transform prePosition;
+    public Text speedText;
+    float delay = 0;
+
+    int a = 1;
 
     float speed;
     void Start()
@@ -18,19 +23,37 @@ public class GetSpeed : MonoBehaviour
     void Update()
     {
         currentPosition = this.transform; // 해당 프레임에서의 위치
+        
 
-        if(prePosition == null) //첫 프레임에서는 이전 포지션이 없으니 예외처리 해준다.
+
+        if (prePosition == null) //첫 프레임에서는 이전 포지션이 없으니 예외처리 해준다.
         {
-            return;
+            
+            prePosition = this.transform;
         }
+        print(currentPosition.position.z);
+        print(prePosition.position.z);
+
         //속도 구하기
-        speed = (currentPosition.position.x - prePosition.position.x) / Time.deltaTime;
-        print(speed); //콘솔에 속도 표시
+        speed = (currentPosition.transform.position - prePosition.transform.position).magnitude / Time.deltaTime;
+        
+        speedText.text = speed.ToString(); //UI에 속도 표시
+
+        delay += Time.deltaTime;
+
+        
+            prePosition = currentPosition;  // 다음프레임의 입장에서는 비교할 프레임이다.
+
+        
+        
+
+        
+            
+        
 
 
 
-
-        prePosition = this.transform;  // 다음프레임의 입장에서는 비교할 프레임이다.
+        
         
     }
 }
