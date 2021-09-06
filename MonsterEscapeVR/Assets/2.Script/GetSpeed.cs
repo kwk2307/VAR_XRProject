61592b7b2253f5889055c8965f200d1f335d7611
yeongjin <yunggini7676@gmail.com>
2021-09-06 16:33:57 +0900

    속도구현
    속도 해결

--- MonsterEscapeVR/Assets/2.Script/GetSpeed.cs
+++ MonsterEscapeVR/Assets/2.Script/GetSpeed.cs
@@ -19,32 +19,56 @@ public class GetSpeed : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 m_LastPosition;
+    public float m_Speed;
+    public Text m_MeterPerSecond, m_KilometersPerHour;
+
+    void FixedUpdate()
     {
-        currentPosition = this.transform; // 해당 프레임에서의 위치
-        
+        m_Speed = GetSpeed2();
 
+        speedText.text = string.Format("{0:00.00} m/s", m_Speed);
+        //m_KilometersPerHour.text = string.Format("{0:00.00} km/h", m_Speed * 3.6f);
+    }
 
-        if (prePosition == null) //첫 프레임에서는 이전 포지션이 없으니 예외처리 해준다.
-        {
-            
-            prePosition = this.transform;
-        }
-        print(currentPosition.position.z);
-        print(prePosition.position.z);
+    float GetSpeed2()
+    {
+        float speed = (((transform.position - m_LastPosition).magnitude) / Time.deltaTime);
+        m_LastPosition = transform.position;
 
-        //속도 구하기
-        speed = (currentPosition.transform.position - prePosition.transform.position).magnitude / Time.deltaTime;
-        
-        speedText.text = speed.ToString(); //UI에 속도 표시
+        return speed;
+    }
 
+    // Update is called once per frame
+    private void Update()
+    {
         delay += Time.deltaTime;
+        if(delay >= 1)
+        {
+            /*
+            currentPosition = this.transform; // 해당 프레임에서의 위치
 
-        
+            print(currentPosition);
+
+            if (prePosition == null) //첫 프레임에서는 이전 포지션이 없으니 예외처리 해준다.
+            {
+                print("111111111111");
+                prePosition = currentPosition;
+                return;
+            }
+            print("cur " + currentPosition.position);
+            print("pre " + prePosition.position);
+
+            //속도 구하기
+            speed = (currentPosition.position - prePosition.position).magnitude;
+            print(speed);
+            speedText.text = speed.ToString(); //UI에 속도 표시
+
+            delay = 0;
+
             prePosition = currentPosition;  // 다음프레임의 입장에서는 비교할 프레임이다.
+            */
+        }
 
-        
-        
     }
 }
