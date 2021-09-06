34f34cf8fc40a3b14d5476b8049480b856fdbd35
kwk2307 <61642951+kwk2307@users.noreply.github.com>
2021-09-06 13:44:49 +0900

    Test: playerMove
    make palyerMove to rigidbody.addforce

--- MonsterEscapeVR/Assets/2.Script/Rowing.cs
+++ MonsterEscapeVR/Assets/2.Script/Rowing.cs
@@ -22,8 +22,7 @@ public class Rowing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
+        
         isRowing = Vector3.Distance(tracker01.transform.position, tracker04.transform.position);  //현 프레임
         //print("isRowing : " + isRowing);
 
@@ -36,24 +35,18 @@ public class Rowing : MonoBehaviour
 
         //로잉기를 당기는지 확인
         //print("트래커의 변화 정도" + (isRowing - isRowingPre));
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * distance);
 
         if ((isRowing - isRowingPre) > 0.03)
         {
             {
-                print(isRowing - isRowingPre);
-                transform.position += new Vector3(1, 0, 0) * distance; // 당긴 것에 비례해 z축으로 이동
-                //this.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * distance);
-
+                //print(isRowing - isRowingPre);
+                //transform.position += new Vector3(1, 0, 0) * distance; // 당긴 것에 비례해 z축으로 이동  
             }
         }
-
-
-
+        
         distance = Vector3.Distance(tracker01.transform.position, tracker04.transform.position);
         
-        
-
-
         isRowingPre = Vector3.Distance(tracker01.transform.position, tracker04.transform.position); // 과거 프레임(다음 프레임 입장에서)
         //print("isRowingPre : " + isRowingPre);
 
