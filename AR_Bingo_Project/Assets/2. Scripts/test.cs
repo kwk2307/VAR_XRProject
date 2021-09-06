e8fbb566a2c52b361f22875cc9a6b73f54718084
jihyun9808 <jihyun9808@nate.com>
2021-09-06 16:17:12 +0900

    지현
    ㅈㅇㅇ

---
+++ AR_Bingo_Project/Assets/2. Scripts/test.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+    private int r;
+    void Start()
+    {
+
+
+    }
+
+
+    public void ButtonClick()
+    {
+        r = Random.Range(1, 17);
+
+        Debug.Log(r);
+
+        foreach (GameObject item in GameObject.Find("Bingoboard").GetComponent<Bingo>().dd)
+        {
+            if (r.ToString() == item.tag)
+            {
+                Debug.Log("print");
+
+               item.GetComponent<Image>().sprite = Resources.Load("circle_2", typeof(Sprite)) as Sprite;
+
+  
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
