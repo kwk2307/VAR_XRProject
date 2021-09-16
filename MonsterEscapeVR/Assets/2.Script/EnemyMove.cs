3ea0d66e91adf6470bf1a5caad97f72d2df2f94c
zeroji0 <89434532+zeroji0@users.noreply.github.com>
2021-09-16 13:59:32 +0900

    ㅇ
    ㅇ

--- MonsterEscapeVR/Assets/2.Script/EnemyMove.cs
+++ MonsterEscapeVR/Assets/2.Script/EnemyMove.cs
@@ -9,14 +9,22 @@ public class EnemyMove : MonoBehaviour
     public float delayCount;
 
     private GameObject player; //플레이어를 담을 변수
+    GameObject endPoint; //종착지를 담을 변수.
     public GameObject GameOverUI; // 게임오버(실패)UI
     public GameObject GameOverUI_player; // 플레이어 캔버스에 있는 게임어보 UI
+
+    public float enterAngDis = 30f; //분노모드로 들어갈 거리 설정
+    GetDistance distance; //겟디스턴스 스크립트에서 디스턴스(플레이어와 엔드포인트 사이의 거리) 가져온다.
+
     void Start()
     {
         //플레이어를 찾아서 담는다
         player = GameObject.Find("Player");
 
         Destroy(GameObject.Find("BGM")); //브금을 지운다. 모드에 맞는 브금이랑 겹치면 안되니깐
+        endPoint = GameObject.Find("EndPoint"); //종착지를 찾아 담는다.
+
+        distance = GameObject.Find("Distance_p").GetComponent<GetDistance>();
         
     }
 
@@ -55,5 +63,8 @@ public class EnemyMove : MonoBehaviour
             GameOverUI_player.SetActive(true);
         }
     }
+
+    //분노모드 돌입 구현
+
     
 }
