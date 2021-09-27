721c0cf857a0833230056ca312e0c8c57e510029
zeroji0 <89434532+zeroji0@users.noreply.github.com>
2021-09-27 16:27:57 +0900

    Revert "Revert "upload assest""
    This reverts commit 1f80c4f312ef03e55a781c145b233056cdc2f5a8.

---
+++ MonsterEscapeVR/Assets/URPWater/Shaders/HLSL/Includes/URPWaterAlpha.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8331f38c0fc9ae86b1059ca751da58981347df84a07105fe27eaccacaae5eb20
+size 1112
---
+++ MonsterEscapeVR/Assets/URPWater/Shaders/HLSL/Includes/URPWaterHelpers.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9eb566726c922bc9369f16becf6dd8d32c612164f4de4c4d0e13fb07b5bd0667
+size 6591
