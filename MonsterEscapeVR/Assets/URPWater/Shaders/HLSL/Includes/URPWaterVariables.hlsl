01cc1d91146206b0c0c9601d941f457282a56460
kwk2307 <61642951+kwk2307@users.noreply.github.com>
2021-09-27 16:14:56 +0900

    upload assest

---
+++ MonsterEscapeVR/Assets/URPWater/Shaders/HLSL/Includes/URPWaterVariables.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:317fe0361c829a80bc09a14377670998ae7b7e3271125ee468b29fc71d469763
+size 7482
---
+++ MonsterEscapeVR/Assets/URPWater/Shaders/VFX/FoamFX.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b73f1a493c45dc5e08e93c4112f4c8a9399ed080640e3843208484abfe21d82b
+size 2074
