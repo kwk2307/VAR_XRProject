33011b51bdc35178dbc3b951591acfbc630a08cf
zeroji0 <89434532+zeroji0@users.noreply.github.com>
2021-10-05 17:42:21 +0900

    Revert "Y"

--- MonsterEscapeVR/Assets/2.Script/Timer.cs
+++ MonsterEscapeVR/Assets/2.Script/Timer.cs
@@ -6,26 +6,16 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public Text timerLabel;
-    public float delayTimer = 0;
-
-    private float time;
+    private float delayTimer = 0;
 
     void Update()
     {
         delayTimer += Time.deltaTime;
-        if(delayTimer >= 3) //카운트다운 3초가 끝나고 타이머가 작동되도록.
+
+        if (delayTimer >= 3) //카운트다운 3초가 끝나고 타이머가 작동되도록.
         {
-            time += Time.deltaTime;
-
-            var minutes = (int)Mathf.Floor(time / 60); //Divide the guiTime by sixty to get the minutes.
-            var seconds = time % 60;//Use the euclidean division for the seconds.
-            var fraction = (time * 100) % 100;
-            
-
-
-            timerLabel.text = string.Format("{0:00} : {1:00} : {2:00}", minutes, seconds, fraction);
-
+            GameMng.Instance.time = Time.deltaTime;
+            UIMng.Instance.update_time();
         }
         
     }
