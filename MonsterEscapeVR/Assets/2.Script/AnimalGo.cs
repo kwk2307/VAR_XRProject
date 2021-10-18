9b09c51d6849ebd75f17faa13c5019ed6e2aa0b4
zeroji0 <89434532+zeroji0@users.noreply.github.com>
2021-10-18 14:40:27 +0900

    Venice

--- MonsterEscapeVR/Assets/2.Script/AnimalGo.cs
+++ MonsterEscapeVR/Assets/2.Script/AnimalGo.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class AnimalGo : MonoBehaviour
 {
-
+    public float speed = 1;
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
         
     }
 
