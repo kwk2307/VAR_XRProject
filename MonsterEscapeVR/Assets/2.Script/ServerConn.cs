9c7d01e0995b23bf84d41657bdfd1c54ec667cb4
kwk2307 <61642951+kwk2307@users.noreply.github.com>
2021-10-19 11:24:11 +0900

    update server scripts
    게임 종료시 사용 칼로리 업데이트 하는 것으로 수정

--- MonsterEscapeVR/Assets/2.Script/ServerConn.cs
+++ MonsterEscapeVR/Assets/2.Script/ServerConn.cs
@@ -8,9 +8,10 @@ public class ServerConn : Singleton<ServerConn>
 {
 
     [SerializeField] private string userCode;
-    [SerializeField] private string useTime;
-    [SerializeField] private string useCal;
-    [SerializeField] private string useDis;
+    public string curWeight;
+    public string goalWeight;
+    public string term;
+
 
     public string[] str; 
     
@@ -103,7 +104,8 @@ public class ServerConn : Singleton<ServerConn>
 
     }
 
-    public IEnumerator SendUpdatetodayDo()
+
+    public IEnumerator SendUpdatetodayDo(string useTime, string useCal, string useDis)
     {
         Dictionary<string, string> dic = new Dictionary<string, string>();
         dic.Add("userCode", userCode);
@@ -121,7 +123,7 @@ public class ServerConn : Singleton<ServerConn>
 
     }
 
-    public IEnumerator SendUpdateallDo()
+    public IEnumerator SendUpdateallDo(string useTime, string useCal, string useDis)
     {
         Dictionary<string, string> dic = new Dictionary<string, string>();
         dic.Add("userCode", userCode);
