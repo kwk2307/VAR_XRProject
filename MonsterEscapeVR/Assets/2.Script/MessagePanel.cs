eb3e195f2343e3c76cb4036b03e068ee22094f30
zeroji0 <89434532+zeroji0@users.noreply.github.com>
2021-10-21 16:24:11 +0900

    상어모드 메세지 수정

--- MonsterEscapeVR/Assets/2.Script/MessagePanel.cs
+++ MonsterEscapeVR/Assets/2.Script/MessagePanel.cs
@@ -56,11 +56,11 @@ public class MessagePanel : MonoBehaviour
     }
     IEnumerator say2()
     {
-        Messagetext.text = "평화로운 리조트에 상어가 나타났다!";
+        Messagetext.text = "배를 타다가 실수로 상어의 영역에 들어간 것 같다..";
         yield return new WaitForSeconds(3);
-        Messagetext.text = "굶주린 상어는 자신의 영역에 외부인이 침입한걸 몹시 불쾌해 하는듯하다...";
+        Messagetext.text = "굶주린 상어는 당장이라도 나를 잡아먹을 것 같다...";
         yield return new WaitForSeconds(3);
-        Messagetext.text = "분노한 상어의 추격을 뿌리치고 육지로 도망쳐라!";
+        Messagetext.text = "굶주린 상어의 추격을 뿌리치고 육지로 도망쳐라!";
         yield return new WaitForSeconds(3);
         messagePanel.SetActive(false);
         SoundMng.Instance.GameStartSound();
