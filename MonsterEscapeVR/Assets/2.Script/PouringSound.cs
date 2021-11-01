d1d876153c6791465bd8fd152d0946c9d88a1fb8
zeroji0 <89434532+zeroji0@users.noreply.github.com>
2021-11-01 14:57:04 +0900

    pouringSound

---
+++ MonsterEscapeVR/Assets/2.Script/PouringSound.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PouringSound : MonoBehaviour
+{
+    public AudioSource pourSound;
+    
+
+    void PouringSoundPlay()
+    {
+        pourSound.Play();
+    }
+}
