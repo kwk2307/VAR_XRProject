98951cd15a410f50d96fe2428850957619558dde
kwk2307 <61642951+kwk2307@users.noreply.github.com>
2021-11-02 10:49:03 +0900

    광원

--- MonsterEscapeVR/Assets/2.Script/KiloSetting.cs
+++ MonsterEscapeVR/Assets/2.Script/KiloSetting.cs
@@ -26,11 +26,7 @@ public class KiloSetting : MonoBehaviour
         curweightText.text = ServerConn.Instance.curWeight;
         goalWeightText.text = ServerConn.Instance.goalWeight;
         goalTermNumText.text = ServerConn.Instance.term;
-
-        curweight = int.Parse(curweightText.text);
-        goalweight = int.Parse(goalWeightText.text);
-        goalTerm = int.Parse(goalTermNumText.text);
-
+        
         curUp.onClick.AddListener(CurweightUp);
         curDown.onClick.AddListener(CurweightDown);
 
@@ -47,33 +43,38 @@ public class KiloSetting : MonoBehaviour
     
     public void CurweightUp()
     {
-
+        curweight = int.Parse(curweightText.text);
         curweight += 1;
         curweightText.text = curweight.ToString();
     }
     public void CurweightDown()
     {
-        if(curweight>0) curweight -= 1;
+        curweight = int.Parse(curweightText.text);
+        if (curweight>0) curweight -= 1;
         curweightText.text = curweight.ToString();
     }
     public void GoalweightUp()
     {
+        goalweight = int.Parse(goalWeightText.text);
         goalweight += 1;
         goalWeightText.text = goalweight.ToString();
     }
     public void GoalweightDown()
     {
-        if(goalweight>0) goalweight -= 1;
+        goalweight = int.Parse(goalWeightText.text);
+        if (goalweight>0) goalweight -= 1;
         goalWeightText.text = goalweight.ToString();
     }
     public void GoalTermUp()
     {
-       goalTerm += 1;
+        goalTerm = int.Parse(goalTermNumText.text);
+        goalTerm += 1;
        goalTermNumText.text = goalTerm.ToString();
     }
     public void GoalTermDown()
     {
-        if(goalTerm>0) goalTerm -= 1;
+        goalTerm = int.Parse(goalTermNumText.text);
+        if (goalTerm>0) goalTerm -= 1;
         goalTermNumText.text = goalTerm.ToString();
     }
     public void Exitscreen()
