6e7f044354d16cdfd8587a28fa5b7691a6d772e5
kwk2307 <61642951+kwk2307@users.noreply.github.com>
2021-11-08 14:19:53 +0900

    update networkMng

--- AR_Bingo_Project/Assets/2. Scripts/NetworkMng.cs
+++ AR_Bingo_Project/Assets/2. Scripts/NetworkMng.cs
@@ -12,12 +12,14 @@ public class NetworkMng : MonoBehaviourPunCallbacks
     [SerializeField] public GameObject LoginPanel;
     [SerializeField] public InputField PlayerName;
 
-    [Header("Login_Before")]
+    [Header("Login_After")]
+    [SerializeField] public GameObject Login_After;
     [SerializeField] public GameObject QuickStart;
     [SerializeField] public GameObject RoomCreate;
     [SerializeField] public GameObject RoomList;
 
-    [Header("Login_After")]
+    [Header("Login_Before")]
+    [SerializeField] public GameObject Login_Before;
     [SerializeField] public GameObject Login;
 
     [Header("Room_Create")]
@@ -34,6 +36,7 @@ public class NetworkMng : MonoBehaviourPunCallbacks
     {
         PhotonNetwork.AutomaticallySyncScene = true;
         PlayerName.text = "Player" + Random.Range(1000, 10000);
+        RoomName.text = "Room" + Random.Range(0, 100);
     }
     #endregion
 
@@ -53,10 +56,26 @@ public class NetworkMng : MonoBehaviourPunCallbacks
         }
     }
 
-    public void MakeRoom()
+    public void CreateRoom()
     {
-
+        if (!RoomName.Equals(""))
+        {
+            if (RoomNum.Equals(""))
+            {
+                PhotonNetwork.CreateRoom(RoomName.text, new RoomOptions { MaxPlayers = byte.Parse(RoomNum.text)});
+            }
+            else
+            {
+                Debug.LogError("Room Num is invalid.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Room Name is invalid.");
+        }
+     
     }
+
     #endregion
 
     #region CALLBACK
@@ -75,13 +94,16 @@ public class NetworkMng : MonoBehaviourPunCallbacks
 
     public override void OnJoinedLobby()
     {
-        base.OnJoinedLobby();
-
+        Login_After.SetActive(true);
+        Login_Before.SetActive(false);
+        LoginPanel.SetActive(false);
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         print("OnDisconnected");
+        Login_After.SetActive(false);
+        Login_Before.SetActive(true);
     }
 
     public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
@@ -89,4 +111,19 @@ public class NetworkMng : MonoBehaviourPunCallbacks
         print("OnPlayerPropertiesUpdate" + targetPlayer.NickName + " " + changedProps["Level"]);
     }
     #endregion
+
+    public override void OnCreatedRoom()
+    {
+        RoomCreatePanel.SetActive(false);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) 
+    {
+        Debug.LogError(message);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) 
+    {
+        Debug.LogError(message);
+    }
 }
