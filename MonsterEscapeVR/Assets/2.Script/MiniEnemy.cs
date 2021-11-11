891751d32f1ef95c9d969184e46e90f83f68883b
zeroji0 <89434532+zeroji0@users.noreply.github.com>
2021-11-11 10:58:42 +0900

    미니언 스크립트
    플레이어를 바라보도록(LookAt)

--- MonsterEscapeVR/Assets/2.Script/MiniEnemy.cs
+++ MonsterEscapeVR/Assets/2.Script/MiniEnemy.cs
@@ -13,8 +13,10 @@ public class MiniEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, 0.3f); //플레이어를 향해 날라간다
-        this.transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time)*10f, transform.position.z);
-        print(Time.time);
+        transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, 0.2f); //플레이어를 향해 날라간다
+        this.transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time)*9f, transform.position.z);//포물선으로 날라가게
+
+        // 진행방향과 앞 벡터가 일치하도록
+        transform.LookAt(player.transform.position);
     }
 }
