a7c672ab7b31af7429bd0ba42d6c545a21dc6ce1
kwk2307 <61642951+kwk2307@users.noreply.github.com>
2021-11-22 09:31:48 +0900

    upload ARProject
    ar project

--- AR_Bingo_Project/Assets/2. Scripts/NetworkMng.cs
+++ AR_Bingo_Project/Assets/2. Scripts/NetworkMng.cs
@@ -9,46 +9,56 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 public class NetworkMng : MonoBehaviourPunCallbacks
 {
     [Header("Login")]
-    [SerializeField] public GameObject LoginPanel;
-    [SerializeField] public InputField PlayerName;
+    [SerializeField] public GameObject loginPanel;
+    [SerializeField] public InputField playerName;
 
     [Header("Login_After")]
-    [SerializeField] public GameObject Login_After;
-    [SerializeField] public GameObject QuickStart;
-    [SerializeField] public GameObject RoomCreate;
-    [SerializeField] public GameObject RoomList;
+    [SerializeField] public GameObject login_After;
+    [SerializeField] public GameObject quickStart;
+    [SerializeField] public GameObject roomCreate;
+    [SerializeField] public GameObject roomList;
 
     [Header("Login_Before")]
-    [SerializeField] public GameObject Login_Before;
-    [SerializeField] public GameObject Login;
+    [SerializeField] public GameObject login_Before;
+    [SerializeField] public GameObject login;
 
     [Header("Room_Create")]
-    [SerializeField] public GameObject RoomCreatePanel;
-    [SerializeField] public InputField RoomName;
-    [SerializeField] public InputField RoomNum;
+    [SerializeField] public GameObject roomCreatePanel;
+    [SerializeField] public InputField roomName;
+    [SerializeField] public InputField roomNum;
 
     [Header("Room_List")]
-    [SerializeField] public GameObject RoomListPanel;
-    [SerializeField] public GameObject RoomInst;
+    [SerializeField] public GameObject roomListPanel;
+    [SerializeField] public GameObject roomListPanel_content;
+    [SerializeField] public GameObject roomInst;
 
+    [Header("Room")]
+    [SerializeField] public GameObject roomPanel;
+    [SerializeField] public GameObject roomPanel_content;
+    [SerializeField] public GameObject userInst;
+
+    List<RoomInfo> myList = new List<RoomInfo>();
 
     #region UNITY
     public void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
-        PlayerName.text = "Player" + Random.Range(1000, 10000);
-        RoomName.text = "Room" + Random.Range(0, 100);
+        playerName.text = "Player" + Random.Range(1000, 10000);
+
+        roomName.text = "Room" + Random.Range(0, 100);
+        roomNum.text = "4";
     }
     #endregion
 
     #region 서버연결
     public void Connect()
     {
-        string playerName = PlayerName.text;
+        string playerName = this.playerName.text;
 
         if (!playerName.Equals(""))
         {
             PhotonNetwork.LocalPlayer.NickName = playerName;
+            print("connect click");
             PhotonNetwork.ConnectUsingSettings();
         }
         else
@@ -56,14 +66,13 @@ public class NetworkMng : MonoBehaviourPunCallbacks
             Debug.LogError("Player Name is invalid.");
         }
     }
-
     public void CreateRoom()
     {
-        if (!RoomName.Equals(""))
+        if (!roomName.Equals(""))
         {
-            if (!RoomNum.Equals(""))
+            if (!roomNum.Equals(""))
             {
-                PhotonNetwork.CreateRoom(RoomName.text, new RoomOptions { MaxPlayers = byte.Parse(RoomNum.text)});
+                PhotonNetwork.CreateRoom(roomName.text, new RoomOptions { MaxPlayers = byte.Parse(roomNum.text)});
             }
             else
             {
@@ -76,10 +85,54 @@ public class NetworkMng : MonoBehaviourPunCallbacks
         }
      
     }
-    public void UpdateRoomList()
+    private void MyRoomListRenewal()
     {
+        var child = roomListPanel_content.GetComponentsInChildren<Transform>();
 
+        foreach (var iter in child)
+        {
+            if (iter != roomListPanel_content.transform)
+            {
+                Destroy(iter.gameObject);
+            }
+        }
+
+        for (int i = 0; i < myList.Count; ++i)
+        {
+            GameObject go = Instantiate(roomInst, roomListPanel_content.transform);
+            go.transform.Find("Name").GetComponent<Text>().text = myList[i].Name;
+            go.transform.Find("Number").GetComponent<Text>().text = myList[i].PlayerCount + "/" + myList[i].MaxPlayers;
+        }
     }
+    private void MyRoomRenawal()
+    {
+        var child = roomPanel_content.GetComponentsInChildren<Transform>();
+
+        foreach (var iter in child)
+        {
+            if (iter != roomPanel_content.transform)
+            {
+                Destroy(iter.gameObject);
+            }
+        }
+
+        foreach (var iter in PhotonNetwork.CurrentRoom.Players)
+        {
+            GameObject go = Instantiate(userInst, roomPanel_content.transform);
+            go.transform.Find("Captain").gameObject.SetActive(iter.Value.IsMasterClient);
+            go.transform.Find("Name").gameObject.GetComponent<Text>().text = iter.Value.NickName;
+        }
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            roomPanel.transform.Find("GameStart").gameObject.SetActive(true);
+        }
+    }
+    public void GameStart()
+    {
+        PhotonNetwork.LoadLevel("GameScene");
+    }
+    public void JoinRandomRoom() => PhotonNetwork.JoinRandomRoom();
 
     #endregion
 
@@ -99,27 +152,26 @@ public class NetworkMng : MonoBehaviourPunCallbacks
 
     public override void OnJoinedLobby()
     {
-        Login_After.SetActive(true);
-        Login_Before.SetActive(false);
-        LoginPanel.SetActive(false);
+        login_After.SetActive(true);
+        login_Before.SetActive(false);
+        loginPanel.SetActive(false);
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         print("OnDisconnected");
-        Login_After.SetActive(false);
-        Login_Before.SetActive(true);
+        login_After.SetActive(false);
+        login_Before.SetActive(true);
     }
 
     public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
     {
         print("OnPlayerPropertiesUpdate" + targetPlayer.NickName + " " + changedProps["Level"]);
     }
-    #endregion
 
     public override void OnCreatedRoom()
     {
-        RoomCreatePanel.SetActive(false);
+        roomCreatePanel.SetActive(false);
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message) 
@@ -135,11 +187,37 @@ public class NetworkMng : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         print("OnJoinedRoom");
+
+        MyRoomRenawal();
+        roomPanel.SetActive(true);
+        
     }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        MyRoomRenawal();
+    }
+    
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        MyRoomRenawal();
+    }
+    
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
-        foreach (RoomInfo info in roomList)
+        int roomCount = roomList.Count;
+        for (int i = 0; i < roomCount; i++)
         {
+            if (!roomList[i].RemovedFromList)
+            {
+                if (!myList.Contains(roomList[i])) myList.Add(roomList[i]);
+                else myList[myList.IndexOf(roomList[i])] = roomList[i];
+            }
+            else if (myList.IndexOf(roomList[i]) != -1) myList.RemoveAt(myList.IndexOf(roomList[i]));
         }
+
+        MyRoomListRenewal();
     }
+    
+    #endregion
 }
