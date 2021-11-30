e1de0d98f2d511cee0195e746f42329971731b7d
kwk2307 <61642951+kwk2307@users.noreply.github.com>
2021-11-30 16:13:55 +0900

    update gameMng
    다른 유저가 클릭한 숫자도 입력 반영되도록

--- AR_Bingo_Project/Assets/2. Scripts/GameMng.cs
+++ AR_Bingo_Project/Assets/2. Scripts/GameMng.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 
 public class GameMng : MonoBehaviourPunCallbacks
@@ -31,5 +32,24 @@ public class GameMng : MonoBehaviourPunCallbacks
     public void Bingo(string str)
     {
         bingo.Add(str);
+        RenewalBingoBoard(bingo);
+        
+    }
+
+    public void RenewalBingoBoard(List<string> bingo)
+    {
+        Transform[] bingoBoard = GameObject.Find("Bingoboard").GetComponentsInChildren<Transform>();
+
+        foreach(var i in bingo)
+        {
+            foreach (var j in bingoBoard)
+            {
+                if(j.GetComponentInChildren<Text>().text == i)
+                {
+                    j.GetComponentInParent<Image>().sprite = Resources.Load("circle_2", typeof(Sprite)) as Sprite;
+                    break;
+                } 
+            }
+        }
     }
 }
